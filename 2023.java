1b9c06b87f8ab1e40f13d5132b17821eb3d25a0d
Selrahc49 <charlesopaleke@yahoo.com>
2023-01-09 09:08:59 -0800

    java script

--- 2023.java
+++ 2023.java
@@ -1,34 +1,7 @@
-import java.util.Calendar;
+import java.util.*;
 
-public class CalendarGenerator {
-  public static void main(String[] args) {
-    int year = 2023; // The year for which the calendar is generated
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(Calendar.YEAR, year);
-    calendar.set(Calendar.MONTH, Calendar.JANUARY);
-    
-    // Print the calendar for the whole year
-    for (int month = 0; month < 12; month++) {
-      calendar.set(Calendar.MONTH, month);
-      System.out.println("\n" + calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Calendar.US) + " " + calendar.get(Calendar.YEAR));
-      System.out.println("Su Mo Tu We Th Fr Sa");
-      
-      // Print the calendar for the current month
-      int firstDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-      for (int i = 0; i < firstDayOfWeek - 1; i++) {
-        System.out.print("   ");
-      }
-      while (calendar.get(Calendar.MONTH) == month) {
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        if (day < 10) {
-          System.out.print(" ");
-        }
-        System.out.print(day + " ");
-        if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
-          System.out.println();
-        }
-        calendar.add(Calendar.DAY_OF_MONTH, 1);
-      }
-    }
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
   }
 }
